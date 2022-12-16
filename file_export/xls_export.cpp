23dd66edd6712f2458419e97f6fc9a66509ea495
Dmitriy Dzyuba <ddzuba@yandex.ru>
2022-12-16 08:27:40 +0000

    some fixes

--- file_export/xls_export.cpp
+++ file_export/xls_export.cpp
@@ -1,11 +1,27 @@
 #include "xls_export.h"
 #include <experimental/filesystem>
 #include <OpenXLSX/OpenXLSX.hpp>
+#include <map>
 
 namespace file_export
 {
     void ExportXLS::start_export(std::vector<report::Report> &report)
     {
+        std::map<std::string,std::string> status;
+
+        status["Бэклог"]="В работе";
+        status["Выполнено"]="Готово";
+        status["Закрыт"]="Готово";
+        status["Открыто"]="В работе";
+        status["Готово к проверке"]="Ревью";
+        status["В ожидании"]="Приостановлено";
+        status["Отклонено"]="Отменено";
+
+        auto map_status = [&status](auto &old_status) -> std::string {
+            if(status.find(old_status)!=std::end(status)){
+                return status[old_status];
+            } else return old_status;
+        };
 
         // create description XLS
         std::string xls_name = "report.xlsx";
@@ -25,7 +41,7 @@ namespace file_export
         wks.cell(1, 5).value() = "Epic name";
         wks.cell(1, 6).value() = "Epic link";
         wks.cell(1, 7).value() = "Status";
-
+        wks.setColor(OpenXLSX::XLColor(120,0,0));
         int i = 2;
         for (report::Report &r : report)
         {
@@ -33,11 +49,13 @@ namespace file_export
             wks.cell(i, 2).value() = r.cluster;
             wks.cell(i, 3).value() = r.product;
             int j = 4;
+
             for(auto [i_status,p_status]: r.issue_status){
                 wks.cell(i, j++).value() = i_status.key;
                 wks.cell(i, j++).value() = i_status.name;
-                wks.cell(i, j++).value() = "https://jira.mts.ru/browse/"+p_status.key;
-                wks.cell(i, j++).value() = p_status.status;
+                wks.cell(i, j++).formula().set("=HYPERLINK(\"https://jira.mts.ru/browse/"+p_status.key+"\")");
+                wks.cell(i, j++).value() = map_status(p_status.status);
+                
                 
             }
             ++i;
--- job.py
+++ job.py
@@ -1,5 +1,5 @@
 import subprocess
-
+import sys
 
 from exchangelib import Credentials
 from exchangelib import DELEGATE, IMPERSONATION, Account
@@ -9,21 +9,29 @@ from exchangelib import FileAttachment
 import time
 import datetime
 
+if len(sys.argv) < 4:
+    print("Usage: python3 job.py email login password")
+    return
+
+email = sys.argv[1]
+login = sys.argv[2]
+password = sys.argv[3]
+
 # if for some reason this script is still running
 # after a year, we'll stop after 365 days
 for i in range(0,365):
     # sleep until 2AM
     t = datetime.datetime.today()
-    future = datetime.datetime(t.year,t.month,t.day,15,16)
-    if t.hour >= 15 and t.minute >=16:
+    future = datetime.datetime(t.year,t.month,t.day,2,0)
+    if t.hour >= 2:
         future += datetime.timedelta(days=1)
     print(f'sleep {(future-t).total_seconds()} seconds')
     time.sleep((future-t).total_seconds())
-    subprocess.run(["./export.sh"])
+    subprocess.run(["/bin/sh","export.sh"])
 
-    credentials = Credentials(username='user', password='password')
+    credentials = Credentials(username=login, password=password)
     my_account = Account(
-        primary_smtp_address='i@yandex.ru', credentials=credentials,
+        primary_smtp_address=email, credentials=credentials,
         autodiscover=True, access_type=DELEGATE
         )
 
@@ -32,10 +40,10 @@ for i in range(0,365):
         subject='Отчет о инициативах'.encode('utf-8'),
         body='',
         to_recipients=[
-            Mailbox(email_address='i@yandex.ru')
+            Mailbox(email_address=email)
         ],  # Or a mix of both
     )
     m.attach(FileAttachment(
-        name='readme.md', content=open('readme.md', 'rb').read())
+        name='report'+t.year+t.month+t.day+'.xlsx', content=open('report.xlsx', 'rb').read())
     )
     m.send()
