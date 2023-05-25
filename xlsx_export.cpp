504af50d5b1608dd90d1df1b3936ab0a50e514e5
Dmitriy Dzyuba <ddzuba@yandex.ru>
2023-05-25 15:02:13 +0300

    fixes

--- xlsx_export.cpp
+++ xlsx_export.cpp
@@ -49,21 +49,21 @@ bool do_init(){
         create_stmt << "CREATE TABLE IF NOT EXISTS HREF(link VARCHAR(512) NOT NULL,title VARCHAR(512), KEY(link));";create_stmt.execute();
         
         std::vector<std::string> inserts{
-            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Cloud Native','KA-1395');",
-            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Cloud Native','KA-1396');",
-            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Cloud Native','KA-1397');",
-            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('ArchOPS','KA-1435');",
-            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('TechRadar','ARC-199');",
-            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Target Architecture Guidelines','KA-1140');",
-            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Target Architecture Guidelines','KA-1141');",
-            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Target Architecture Guidelines','KA-1142');",
-            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Near Realtime','KA-1480');",
-            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Near Realtime','KA-1504');",
-            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Micro-frontends for Web UIs','KA-2234');",
-            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Пилотирование практики Architecture as Code','KA-2227');",
-            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Запрет интеграции через БД','KA-2217');",
-            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Реализация задач по переходу с технологий в статусе HOLD - 23Q1-23Q4','ARC-200');",
-            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('[BCAA] Актуализация описания компетенций и связей с ПО','KA-2255');"
+            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Cloud Native','KA-1395')",
+            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Cloud Native','KA-1396')",
+            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Cloud Native','KA-1397')",
+            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('ArchOPS','KA-1435')",
+            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('TechRadar','ARC-199')",
+            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Target Architecture Guidelines','KA-1140')",
+            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Target Architecture Guidelines','KA-1141')",
+            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Target Architecture Guidelines','KA-1142')",
+            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Near Realtime','KA-1480')",
+            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Near Realtime','KA-1504')",
+            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Micro-frontends for Web UIs','KA-2234')",
+            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Пилотирование практики Architecture as Code','KA-2227')",
+            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Запрет интеграции через БД','KA-2217')",
+            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('Реализация задач по переходу с технологий в статусе HOLD - 23Q1-23Q4','ARC-200')",
+            "INSERT INTO Initiatives_Issue(initative_name,issue_key) VALUES ('[BCAA] Актуализация описания компетенций и связей с ПО','KA-2255')"
         };
         std::cout << "check records" << std::endl;
         long count{0};
